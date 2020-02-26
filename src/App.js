04fd0857c5ac8cd477db8a32ca290f7c76823949
Amdoouni Slim <amdouni.slim@gmail.com>
2020-02-26 11:05:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './App.css';
+import MovieApp from './MovieApp';
+import WithLoading from './WithLoading'
+
+const ListWithLoading = WithLoading(MovieApp);
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading : false
+    };
+  }
+
+
+  componentDidMount=() => {
+    setTimeout(()=>{
+      this.setState({ loading : !this.state.loading });
+    }, 10000);
+  } 
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <div className="movie-app">
+            <ListWithLoading  isLoading={this.state.loading}/>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
